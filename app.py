c55101145f31700b498e4d07be4d5bd9d27402f1
ibrahim-asistan <saloonmakass3654@gmail.com>
2026-07-28 19:10:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ else:
 
     try:
       response = client.models.generate_content(
-          model="gemini-1.5-flash", contents=prompt
+          model="gemini-2.5-flash", contents=prompt
       )
       bot_response = response.text
     except Exception as e:
